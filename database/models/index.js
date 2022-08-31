f8af3585cf804a14e659d525f8b763ba57b356c0
𝙹𝚒 𝙼𝚒𝚗 <jimny@ui-MacBookPro.local>
2022-08-31 16:15:49 +0900

    CREATE: modeling

---
+++ database/models/index.js
@@ -0,0 +1,38 @@
+const Sequelize = require("sequelize");
+const env = process.env.NODE_ENV || "development";
+const config = require("../config")[env];
+
+const Category = require("./category");
+const FreeBoard = require("./freeBoard");
+const NoticeBoard = require("./noticeBoard");
+const User = require("./user");
+const OperateBoard = require("./operateBoard");
+
+const db = {};
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+db.sequelize = sequelize;
+db.User = User;
+db.FreeBoard = FreeBoard;
+db.NoticeBoard = NoticeBoard;
+db.OperateBoard = OperateBoard;
+db.Category = Category;
+
+User.init(sequelize);
+FreeBoard.init(sequelize);
+NoticeBoard.init(sequelize);
+OperateBoard.init(sequelize);
+Category.init(sequelize);
+
+User.associate(db);
+NoticeBoard.associate(db);
+FreeBoard.associate(db);
+OperateBoard.associate(db);
+Category.associate(db);
+
+module.exports = db;
---
+++ database/models/noticeBoard.js
@@ -0,0 +1,34 @@
+const Sequelize = require("sequelize");
+
+module.exports = class NoticeBoard extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        title: {
+          type: Sequelize.STRING(100),
+          allowNull: false,
+          unique: false,
+        },
+        content: {
+          type: Sequelize.STRING(100),
+          allowNull: false,
+          unique: false,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: "NoticeBoard",
+        tableName: "noticeBoards",
+        paranoid: true,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+
+  static associate(db) {
+    db.NoticeBoard.belongsTo(db.User);
+  }
+};
