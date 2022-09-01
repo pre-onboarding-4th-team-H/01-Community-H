f5aee747b255cac3cf3342e42cb7e7a60ffd34ce
JoKyungseo <eksql0645@gmail.com>
2022-09-01 21:00:02 +0900

    Feat: Add checking login and modifying user variable

--- middlewares/adminRequired.js
+++ middlewares/adminRequired.js
@@ -1,31 +1,25 @@
-const models = require("../database/models");
-
-// 임시 미들웨어
-
-// 관리자 등급 확인
+// 로그인 및 관리자 등급 확인 미들웨어
 const adminRequired = async (req, res, next) => {
   try {
-    // 로그인 과정을 거쳤다고 가정
-    const { UserId } = req.body;
+    // 로그인했다면 req.user에 user 정보가 담김
+    const user = req.user;
 
-    // 회원 등급 조회
-    const user = await models.User.findOne({ where: { id: UserId } });
+    // 로그인을 하지 않은 경우
+    if (!user) {
+      throw new Error("로그인이 필요한 페이지입니다.");
+    }
 
     // 관리자가 아닌 경우
     if (user.tier !== 0) {
-      throw new Error();
+      throw new Error("관리자만 사용할 수 있는 서비스입니다.");
     }
 
     // 게시글 수정/ 삭제 시 passowrd를 사용하기 위해 req.pass에 할당
     req.pass = user.password;
 
     next();
-  } catch (e) {
-    res.status(403).json({
-      result: "forbidden-approach",
-      reason: "관리자만 사용할 수 있는 서비스입니다.",
-    });
-    return;
+  } catch (err) {
+    next(err);
   }
 };
 
