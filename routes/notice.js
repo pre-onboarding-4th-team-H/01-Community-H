48b4e792ac85bdada318659cdf5395b483cef291
JoKyungseo <eksql0645@gmail.com>
2022-09-01 22:13:38 +0900

    Style: Changeing order of codes

--- routes/notice.js
+++ routes/notice.js
@@ -6,14 +6,14 @@ const {
   setPost,
   deletePost,
 } = require("../services/notice");
-const router = Router();
 const adminRequired = require("../middlewares/adminRequired");
 
-// 관리자 CRUD / 유저 R
+const router = Router();
 
+// 관리자 CRUD / 유저 R
 router.post("/notice", adminRequired, addPost);
-router.get("/notice", getPosts);
 router.get("/notice/:id", getPost);
+router.get("/notice", getPosts);
 router.patch("/notice/:id", adminRequired, setPost);
 router.delete("/notice/:id", adminRequired, deletePost);
 
