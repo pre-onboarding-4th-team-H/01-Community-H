4bdeec517a3c875a35b8366f870a87d122361086
Lee-moo <dlandif22@gmail.com>
2022-09-01 22:08:57 +0900

    Feat Create User API - service
    - service에서는 사용자 생성을 위해 repository에 접근하기 전
    - Repository에 맞는 데이터 변환한 후에 repository를 호출한다.

---
+++ services/users.js
@@ -0,0 +1,14 @@
+const { usersRepository } = require("../repos");
+const userJoinDao = require("../dao/userJoinDao");
+
+// service 로직은 사용자 데이터를 repository에서 실제 DB에 접근하기 전
+// data를 변환 후 repository에 addUser를 요청한다.
+const addUser = async (data) => {
+  try {
+    const user = await usersRepository.addUser(await userJoinDao(data));
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = { addUser };
