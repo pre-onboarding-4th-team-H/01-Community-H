40f27eb3d55af2764842f3bf717cd4d7b2f5a37d
Lee-moo <dlandif22@gmail.com>
2022-09-02 11:01:11 +0900

    Refactor change service for deleting controller

--- services/users.js
+++ services/users.js
@@ -1,13 +1,13 @@
 const { usersRepository } = require("../repos");
 const userJoinDao = require("../dao/userJoinDao");
-
 // service 로직은 사용자 데이터를 repository에서 실제 DB에 접근하기 전
 // data를 변환 후 repository에 addUser를 요청한다.
-const addUser = async (data) => {
+const addUser = async (req, res, next) => {
   try {
-    const user = await usersRepository.addUser(await userJoinDao(data));
+    const user = await usersRepository.createUser(await userJoinDao(req.body));
+    res.status(201).end();
   } catch (err) {
-    throw err;
+    next(err);
   }
 };
 
