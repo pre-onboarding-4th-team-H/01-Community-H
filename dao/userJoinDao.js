e5a98c0baea8a23c65c1cbba3516056cb4f805dc
Lee-moo <dlandif22@gmail.com>
2022-09-02 10:53:40 +0900

    Update change modeling for security
    모든 모델링의 id(PK)를 기존 Integer에서 UUID로 보안을 위해 변경하기로 결정
    현재 브랜치는 유저 생성만 담당하기 때문에 해당하는 User Table의 스키마와 create 로직 변경

--- dao/userJoinDao.js
+++ dao/userJoinDao.js
@@ -1,7 +1,8 @@
 const bcrypt = require("bcrypt");
-
+const { v4: uuidv4 } = require("uuid");
 module.exports = async (body) => {
   return {
+    id: uuidv4(),
     name: body.name,
     email: body.email,
     password: await bcrypt.hash(body.password, 12),
