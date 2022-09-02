48bf9e2f999afd6b235819bafda139f65d54d9fb
KIMYECHAN <kyc5244@gmail.com>
2022-09-02 16:52:20 +0900

    validator index 충돌 해결

--- middlewares/validator/index.js
+++ middlewares/validator/index.js
@@ -1,13 +1,10 @@
 const { validationResult } = require("express-validator");
 
-<<<<<<< HEAD
-=======
 /**
  * 각 검증기의 결과를 req에서 조회 후
  * error가 존재한다면 return
  * error가 존재하지 않다면 계속해서 controller에서 진행한다.
  */
->>>>>>> develop
 module.exports = (req, res, next) => {
   const errors = validationResult(req);
   if (errors.isEmpty()) {
@@ -15,19 +12,11 @@ module.exports = (req, res, next) => {
   }
 
   const resultErrors = { errors: [] };
-<<<<<<< HEAD
-=======
-
->>>>>>> develop
   errors.array().forEach((error) => {
     resultErrors.errors.push({
       field: error.param,
       message: error.msg,
     });
   });
-<<<<<<< HEAD
-=======
-
->>>>>>> develop
   return res.status(400).json(resultErrors);
 };
