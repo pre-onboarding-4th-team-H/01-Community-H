8ed749acc8bc5735793830a112b7b032e466ee2e
Taekwon <dev.taekwonlee@gmail.com>
2022-09-02 16:34:28 +0900

    Fix: app.js

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const db = require("./database/models");
-const routes = require("./routes");
+const dotenv = require("dotenv");
+// const { swaggerUi, specs } = require("./swagger");
+
 const errorHandler = require("./middlewares/errorHandler");
+const routes = require("./routes");
+
+dotenv.config();
 
 const app = express();
+
 const env = process.env;
 const PORT = env.PORT || 8080;
 
@@ -20,10 +26,14 @@ db.sequelize
   });
 
 app.use(cors());
-app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(morgan("dev"));
 app.use(express.json());
-app.use("/", routes);
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+app.use(morgan("dev"));
+
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+app.use(routes);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
