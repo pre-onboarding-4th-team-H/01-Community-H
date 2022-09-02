8d5be7e5ca505c17310c59dc2b8f6dfa60f2b8a1
KIMYECHAN <kyc5244@gmail.com>
2022-09-02 16:45:18 +0900

    database data sql 파일 제거

--- database/data/user.sql
+++
@@ -1,4 +0,0 @@
--- Active: 1662001520678@@127.0.0.1@3306@waynehills
-INSERT INTO users(name,email,password,age,tier,sex,phoneNumber) VALUES ('예찬','abc@naver.com','123',20,0,true,'01012341234');
-INSERT INTO users(name,email,password,age,tier,sex,phoneNumber) VALUES ('철수','abcd@naver.com','123',20,1,true,'01012341234');
-INSERT INTO users(name,email,password,age,tier,sex,phoneNumber) VALUES ('철민','abcde@naver.com','123',20,2,false,'0101234123');
