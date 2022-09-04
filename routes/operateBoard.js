3c36caf16a9504ccacce36b5bc62e6956c10383a
Lee-moo <dlandif22@gmail.com>
2022-09-04 22:26:44 +0900

    Change operateBoard API over all
    - 지난 세션에서 멘토님이 운영게시판은 운영자들만 CRUD할 수 있도록 하는 게 좋다는 말씀을 하셔서
    - 각 API에 loginRequired와 adminRequired를 추가했습니다.

--- routes/operateBoard.js
+++ routes/operateBoard.js
@@ -1,23 +1,43 @@
 const express = require("express");
 const router = express.Router();
 const loginRequired = require("../middlewares/loginRequired");
-const operateBoardService = require("../services/operateBoard.js");
+const adminRequired = require("../middlewares/adminRequired");
+const { operateBoardService } = require("../services");
 const { boardValidator } = require("../middlewares/validator/boardValidator");
 
 router.post(
   "/",
   loginRequired,
+  adminRequired,
   boardValidator(),
   operateBoardService.addOperateBoard
 );
-// router.post(
-//   "/",
-//   boardValidator(),
-//   operateBoardService.addOperateBoard
-// );
-router.get("/", operateBoardService.getOperateBoards);
-router.get("/:id", operateBoardService.getOperateBoard);
-router.patch("/:id", boardValidator(), operateBoardService.setOperateBoard);
-router.delete("/:id", boardValidator(), operateBoardService.deleteOperateBoard);
+// router.post("/", boardValidator(), operateBoardService.addOperateBoard);
+router.get(
+  "/",
+  loginRequired,
+  adminRequired,
+  operateBoardService.getOperateBoards
+);
+router.get(
+  "/:id",
+  loginRequired,
+  adminRequired,
+  operateBoardService.getOperateBoard
+);
+router.patch(
+  "/:id",
+  loginRequired,
+  adminRequired,
+  boardValidator(),
+  operateBoardService.setOperateBoard
+);
+router.delete(
+  "/:id",
+  loginRequired,
+  adminRequired,
+  boardValidator(),
+  operateBoardService.deleteOperateBoard
+);
 
 module.exports = router;
