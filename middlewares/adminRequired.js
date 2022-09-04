2064ed669c12b3fba166d54e3bfe0c148f44addd
JoKyungseo <eksql0645@gmail.com>
2022-09-04 17:53:04 +0900

    Fix: Modify to getting token

--- middlewares/adminRequired.js
+++ middlewares/adminRequired.js
@@ -1,19 +1,20 @@
+const jwt = require("jsonwebtoken");
+
 // 로그인 및 관리자 등급 확인 미들웨어
 const adminRequired = async (req, res, next) => {
   try {
-    // 로그인했다면 req.user에 user 정보가 담김
-    const user = req.user;
+    // 로그인했다면 req.headers["authorization"]에 담긴 토큰을 가져옴
+    const token = req.headers["authorization"]?.split(" ")[1];
 
-    // 로그인을 하지 않은 경우
-    if (!user) {
-      throw new Error("로그인이 필요한 페이지입니다.");
+    // 해당 token 이 정상적인 token인지 확인
+    const secretKey = process.env.JWT_SECRET_KEY || "secret-key";
+    const jwtDecoded = jwt.verify(token, secretKey);
+    const tier = jwtDecoded.tier;
+
+    // tier 확인
+    if (tier !== 0) {
+      throw new Error("관리자만 접근 가능한 페이지입니다.");
     }
-
-    // 관리자가 아닌 경우
-    if (user.tier !== 0) {
-      throw new Error("관리자만 사용할 수 있는 서비스입니다.");
-    }
-
     next();
   } catch (err) {
     next(err);
