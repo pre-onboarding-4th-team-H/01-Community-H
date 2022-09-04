7357cf1a2d2345f99c90d3c72a71c4b8a3bb7e55
Lee-moo <dlandif22@gmail.com>
2022-09-04 22:23:43 +0900

    Change add loginRequired when requesting patch, delete in freeBoards
    - 자유게시판의 수정과 삭제 시 로그인된 사용자인 지 확인하기 위해서 loginRequired 미들웨어 추가

--- routes/freeBoard.js
+++ routes/freeBoard.js
@@ -6,8 +6,8 @@ const { boardValidator } = require("../middlewares/validator/boardValidator");
 
 router.get("/", freeBoardService.getPosts);
 router.get("/:id", freeBoardService.getPost);
-router.patch("/", boardValidator(), freeBoardService.setPost);
-router.delete("/:id", freeBoardService.deletePost);
+router.patch("/", loginRequired, boardValidator(), freeBoardService.setPost);
+router.delete("/:id", loginRequired, freeBoardService.deletePost);
 router.post("/", loginRequired, boardValidator(), freeBoardService.addPost);
 // router.post("/", boardValidator(), freeBoardService.addPost);
 
