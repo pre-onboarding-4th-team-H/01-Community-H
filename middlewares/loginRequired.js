05f4b90c4364d7f65c33eb0e82e66a82fccaa84d
Lee-moo <dlandif22@gmail.com>
2022-09-04 22:19:34 +0900

    Refactor loginRequired and adminRequired
    - loginRequired에서는 로그인된 사용자로 확인되면 Repository에서 user 조회 후 req.user에 할당하는 방식으로 변경
    - adminRequired는 loginRequired에서 담아준 req.user에서 tier를 확인하는 방식으로 변경

--- middlewares/loginRequired.js
+++ middlewares/loginRequired.js
@@ -5,6 +5,7 @@ const loginRequired = async (req, res, next) => {
   try {
     // 로그인했다면 req.headers["authorization"]에 담긴 토큰을 가져옴
     const token = req.headers["authorization"]?.split(" ")[1];
+
     if (!token || token === "null") {
       throw new Error("로그인이 필요한 페이지입니다.");
     }
@@ -14,9 +15,8 @@ const loginRequired = async (req, res, next) => {
     const jwtDecoded = jwt.verify(token, secretKey);
 
     // 라우터에서 쓸 수 있도록 req.user에 user 저장
-    req.user = await userRepository.findOneWithId(jwtDecoded.userId);
-    console.log(req.user);
-    next;
+    req.user = await userRepository.findUserWithId(jwtDecoded.userId);
+    next();
   } catch (err) {
     next(err);
   }
