7cfe571dd6628188f42400ed2353dd593436dda2
Lee-moo <dlandif22@gmail.com>
2022-09-05 10:26:15 +0900

    Feat add Authenticate in freeBoard API over all

--- routes/freeBoard.js
+++ routes/freeBoard.js
@@ -3,12 +3,20 @@ const router = express.Router();
 const { freeBoardService } = require("../services");
 const loginRequired = require("../middlewares/loginRequired");
 const { boardValidator } = require("../middlewares/validator/boardValidator");
+const {
+  passwordValidator,
+} = require("../middlewares/validator/passwordValidator");
 
 router.get("/", freeBoardService.getPosts);
 router.get("/:id", freeBoardService.getPost);
-router.patch("/", loginRequired, boardValidator(), freeBoardService.setPost);
+router.patch(
+  "/:id",
+  loginRequired,
+  boardValidator(),
+  passwordValidator(),
+  freeBoardService.setPost
+);
 router.delete("/:id", loginRequired, freeBoardService.deletePost);
 router.post("/", loginRequired, boardValidator(), freeBoardService.addPost);
-// router.post("/", boardValidator(), freeBoardService.addPost);
 
 module.exports = router;
